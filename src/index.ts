da89c7dbfa01ebfb6a3e60912e29f11bea8b57f7
Yuki <admin@yuki.sh>
2022-03-09 16:03:56 +0800

    feat: expose bot self id on message events

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,21 @@ declare global {
   var __workname: string;
 }
 
+declare module "oicq" {
+  export interface GroupMessageEvent {
+    self_id: number;
+  }
+  export interface PrivateMessageEvent {
+    self_id: number;
+  }
+  export interface DiscussMessageEvent {
+    self_id: number;
+  }
+}
+
 global.__workname = process.cwd();
 
 export { AllMessageEvent, Bot, startup } from './bot';
 export { Extension, Order } from './plugin';
 export { Option, getOption } from './setting';
-export { colors, logger, section, checkOrder } from './util';
+export { colors, logger, section, getOrder } from './util';
