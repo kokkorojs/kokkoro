f911b05786da17187fb62435a4f12a12fdbe6ad4
Yuki <admin@yuki.sh>
2022-04-10 22:58:40 +0800

    improved setting write

--- src/events.ts
+++ src/events.ts
@@ -11,39 +11,39 @@ declare module 'oicq' {
   export interface DiscussMessageEvent {
     self_id: number;
   }
-  //   //   export interface FriendIncreaseEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface FriendDecreaseEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface FriendRecallEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface FriendPokeEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface MemberIncreaseEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface MemberDecreaseEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface GroupRecallEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface GroupAdminEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface GroupMuteEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface GroupTransferEvent {
-  //   //     self_id: number;
-  //   //   }
-  //   //   export interface GroupPokeEvent {
-  //   //     self_id: number;
-  //   //   }
+  //   export interface FriendIncreaseEvent {
+  //     self_id: number;
+  //   }
+  //   export interface FriendDecreaseEvent {
+  //     self_id: number;
+  //   }
+  //   export interface FriendRecallEvent {
+  //     self_id: number;
+  //   }
+  //   export interface FriendPokeEvent {
+  //     self_id: number;
+  //   }
+  //   export interface MemberIncreaseEvent {
+  //     self_id: number;
+  //   }
+  //   export interface MemberDecreaseEvent {
+  //     self_id: number;
+  //   }
+  //   export interface GroupRecallEvent {
+  //     self_id: number;
+  //   }
+  //   export interface GroupAdminEvent {
+  //     self_id: number;
+  //   }
+  //   export interface GroupMuteEvent {
+  //     self_id: number;
+  //   }
+  //   export interface GroupTransferEvent {
+  //     self_id: number;
+  //   }
+  //   export interface GroupPokeEvent {
+  //     self_id: number;
+  //   }
 }
 
 export const emitter = new EventEmitter();
