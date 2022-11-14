83eadadff9c5e27b1a84305c145080da735d3ca4
yuki <mail@yuki.sh>
2022-11-14 17:42:23 +0800

    feat: large improve

--- src/config/profile.ts
+++ src/config/profile.ts
@@ -1,46 +1,47 @@
 import { join } from 'path';
+import { EventEmitter } from 'events';
 import { writeFileSync } from 'fs';
 import { writeFile } from 'fs/promises';
 import { MemberDecreaseEvent, MemberIncreaseEvent, event } from 'amesu';
 
 import { Bot } from '@/core';
-import { PluginSetting } from '@/plugin';
-import { debounce, deepClone, deepMerge } from '@/utils';
+import { Option } from '@/plugin';
+import { deepClone, deepMerge } from '@/utils';
 
 /** 群聊 */
 export type Group = {
   /** 群名称 */
   name: string;
   /** 插件 */
-  plugin: {
-    /** 插件名 */
-    [name: string]: PluginSetting;
-  }
+  setting: Setting;
 }
 
-export interface BindSettingEvent {
-  name: string;
-  setting: PluginSetting;
+/** 群设置 */
+export type Setting = {
+  /** 插件名 */
+  [name: string]: Option;
 }
 
-export class Profile {
+export class Profile extends EventEmitter {
   disable: Set<string>;
   group: {
     [group_id: number]: Group;
   };
-  defaultSetting: {
-    [key: string]: PluginSetting;
+  defaultOption: {
+    [key: string]: Option;
   };
   readonly file: string;
 
   constructor(
     private bot: Bot,
   ) {
+    super();
+
     const dir = join(this.bot.dir, `profile-${this.bot.uin}.json`);
     const file = join(__workname, dir);
 
     this.file = file;
-    this.defaultSetting = {};
+    this.defaultOption = {};
 
     try {
       const profile: Profile = require(file);
@@ -51,29 +52,44 @@ export class Profile {
       this.group = {};
       this.disable = new Set();
 
-      writeFileSync(file, '{}');
+      const defaultFile = {
+        group: {},
+        disable: [],
+      };
+
+      writeFileSync(file, JSON.stringify(defaultFile, null, 2));
       this.bot.logger.mark("创建了新的配置文件：" + dir);
     }
-    this.bindEvents();
+    this.initEmitter();
+    this.initSubscribe();
   }
 
   /**
-   * 绑定事件监听
+   * 初始化事件触发器
    */
-  private bindEvents(): void {
+  private initEmitter() {
+    this
+      .on('profile.option.init', (event) => {
+        const { plugin_name, option } = event;
+        this.defaultOption[plugin_name] = option;
+      })
+      .on('profile.refresh', () => {
+        this.onRefresh();
+      })
+  }
+
+  /**
+   * 初始化事件订阅
+   */
+  private initSubscribe(): void {
     this.bot
       .pipe(
-        event<any>('profile.bind.setting')
-      )
-      .subscribe(event => this.onBindSetting(event))
-    this.bot
-      .pipe(
-        event<any>('notice.group.increase')
+        event('notice.group.increase')
       )
       .subscribe(event => this.onGroupIncrease(event))
     this.bot
       .pipe(
-        event<any>('notice.group.decrease')
+        event('notice.group.decrease')
       )
       .subscribe(event => this.onGroupDecrease(event))
   }
@@ -91,35 +107,33 @@ export class Profile {
     return writeFile(this.file, JSON.stringify(data, null, 2));
   }
 
-  private async onBindSetting(event: BindSettingEvent) {
-    const { name, setting } = event;
+  private async onRefresh(): Promise<void> {
+    const options = Object.keys(this.defaultOption);
+    const options_length = options.length;
 
-    // 内置插件不用初始化配置
-    if (name === 'kokkoro') {
-      return;
-    }
-    this.defaultSetting[name] = setting;
+    for (let i = 0; i < options_length; i++) {
+      const name = options[i];
+      const option = this.getDefaultOption(name);
 
-    for (const [, info] of this.bot.gl) {
-      const { group_id, group_name } = info;
+      for (const [, info] of this.bot.gl) {
+        const { group_id, group_name } = info;
 
-      this.group[group_id] ??= {
-        name: group_name, plugin: {},
-      };
-      this.group[group_id].name = group_name;
-      this.group[group_id].plugin[name] = deepMerge(
-        deepClone(setting),
-        this.group[group_id].plugin[name]
-      );
+        this.group[group_id] ??= {
+          name: group_name, setting: {},
+        };
+        this.group[group_id].name = group_name;
+        this.group[group_id].setting[name] = deepMerge(
+          deepClone(option),
+          this.group[group_id].setting[name],
+        );
+      }
     }
 
     try {
       await this.write();
-      this.bot.logger.info(`更新了群 ${name} 配置`);
+      this.bot.logger.info(`更新了群配置`);
     } catch (error) {
-      if (error instanceof Error) {
-        this.bot.logger.error(`更新群 ${name} 配置失败，${error.message}`);
-      }
+      this.bot.logger.error(`更新群配置失败，${(<Error>error).message}`);
     }
   }
 
@@ -129,23 +143,24 @@ export class Profile {
     }
     const group_id = event.group_id;
     const group_name = event.group.info!.group_name;
+    // TODO ⎛⎝≥⏝⏝≤⎛⎝ 刚加的新群好像获取不到 group_name（给 oicq 提 pr?
     // const group_name = (await this.getGroupInfo(group_id)).group_name;
 
     this.group[group_id] ??= {
-      name: group_name, plugin: {},
+      name: group_name, setting: {},
     };
     this.group[group_id].name = group_name;
 
-    const settingNames = Object.keys(this.defaultSetting);
+    const settingNames = Object.keys(this.defaultOption);
     const settings_length = settingNames.length;
 
     for (let i = 0; i < settings_length; i++) {
-      const name: string = settingNames[i];
-      const setting: PluginSetting = this.getDefaultSetting(name);
+      const name = settingNames[i];
+      const option = this.defaultOption[name];
 
-      this.group[group_id].plugin[name] = deepMerge(
-        setting,
-        this.group[group_id].plugin[name]
+      this.group[group_id].setting[name] = deepMerge(
+        option,
+        this.group[group_id].setting[name]
       )
     }
 
@@ -174,36 +189,7 @@ export class Profile {
       })
   }
 
-  // private refresh = debounce(async (name: string, setting: PluginSetting) => {
-  //   for (const [, info] of this.bot.gl) {
-  //     const { group_id, group_name } = info;
-
-  //     this.group[group_id] ??= {
-  //       name: group_name, plugin: {},
-  //     };
-  //     this.group[group_id].name = group_name;
-  //     // 刷新指定插件配置项
-  //     this.group[group_id].plugin[name] = deepMerge(
-  //       deepClone(setting),
-  //       this.group[group_id].plugin[name]
-  //     )
-  //   }
-
-  //   try {
-  //     await this.write();
-  //     this.bot.logger.info(`更新了群配置`);
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       this.bot.logger.error(`更新群配置失败，${(error).message}`);
-  //     }
-  //   }
-  // }, 10)
-
-  getDefaultSetting(name: string): PluginSetting {
-    return deepClone(this.defaultSetting[name]);
-  }
-
-  getSetting(group_id: number, name: string) {
-    return this.group[group_id].plugin[name];
+  getDefaultOption(name: string): Option {
+    return deepClone(this.defaultOption[name]);
   }
 }
