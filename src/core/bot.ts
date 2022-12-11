6f14fb5d9cbe54f8bab81feea2feaa11ed82190f
Miaow233 <miaow233@qq.com>
2022-12-11 20:56:42 +0800

    feat: 完善验证码登录流程

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -306,17 +306,17 @@ export class Bot extends Client {
     this
       .on('system.login.slider', () => {
         this.logger.mark('取 ticket 教程: https://github.com/takayama-lily/oicq/wiki/01.滑动验证码和设备锁');
-
-        process.stdout.write('请输入 ticket : ');
+        process.stdout.write('请输入 ticket: ');
         process.stdin.once('data', (event: string) => {
-          this.submitSlider(event);
+          this.submitSlider(event.trim());
         });
       })
       .on('system.login.device', () => {
         // TODO ／人◕ ‿‿ ◕人＼ 设备锁轮询，oicq 暂无相关 func
-        this.logger.mark('验证完成后按回车键继续...');
-        process.stdin.once('data', () => {
-          this.login();
+        this.logger.mark('输入密保手机收到的短信验证码后按回车键继续');
+        this.sendSmsCode();
+        process.stdin.once('data', (event: string) => {
+          this.submitSmsCode(event);
         });
       })
   }
