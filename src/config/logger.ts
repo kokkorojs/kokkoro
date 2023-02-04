d99d5ebb1d9cd6c940f39c98268e2283c646e49b
Yuki <admin@yuki.sh>
2023-02-04 18:48:31 +0800

    feat: add logger

---
+++ src/config/logger.ts
@@ -0,0 +1,36 @@
+import { join } from 'path';
+import { configure } from 'log4js';
+import { getConfig } from '@/config';
+
+const log_level = getConfig('log_level');
+
+configure({
+  appenders: {
+    everything: {
+      filename: join(__logsname, 'kokkoro.log'),
+      keepFileExt: true,
+      type: 'dateFile',
+    },
+    emergencies: {
+      filename: join(__logsname, 'error.log'),
+      type: 'file',
+    },
+    terminal: {
+      type: 'stdout',
+    },
+
+    'just-errors': {
+      type: 'logLevelFilter',
+      appender: 'emergencies',
+      level: 'error',
+    },
+    'just-outputs': {
+      type: 'logLevelFilter',
+      appender: 'terminal',
+      level: log_level,
+    },
+  },
+  categories: {
+    default: { appenders: ['everything', 'just-errors', 'just-outputs'], level: 'all' },
+  },
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { resolve } from 'path';
-import { Logger, getLogger } from 'log4js';
+import { getLogger } from 'log4js';
 
 interface Package {
   author: string;
@@ -9,16 +8,6 @@ interface Package {
   version: string;
 }
 
-declare global {
-  /** 当前进程目录 */
-  var __workname: string;
-  /** 资源目录 */
-  var __dataname: string;
-}
-
-global.__workname = process.cwd();
-global.__dataname = resolve('data');
-
 const { author, changelogs, license, upday, version } = require('../package.json') as Package;
 
 export const AUTHOR = author;
@@ -27,7 +16,7 @@ export const LICENSE = license;
 export const UPDAY = upday;
 export const VERSION = version;
 
-export const logger: Logger = getLogger('[kokkoro]');
+export const logger = getLogger('[kokkoro]');
 
 export * from 'oicq';
 export * from '@/core';
