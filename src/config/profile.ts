db7b86b22788534b4295892af015d8f3a66588f1
yuki <mail@yuki.sh>
2023-02-05 16:52:44 +0800

    feat: add web and admin

---
+++ src/config/profile.ts
@@ -0,0 +1,256 @@
+import { join } from 'path';
+import { writeFileSync } from 'fs';
+import { writeFile } from 'fs/promises';
+import { MemberIncreaseEvent, MemberDecreaseEvent } from 'oicq';
+
+import { Bot } from '@/core';
+import { Option } from '@/plugin';
+import { decache } from '@kokkoro/utils';
+import { deepClone, deepAssign } from '@kokkoro/utils';
+
+/** 群设置 */
+export interface Setting {
+  /** 插件名 */
+  [name: string]: Option;
+}
+
+export class Profile {
+  /** 文件路径 */
+  private readonly file: string;
+  /** 插件默认配置项 */
+  private defaultSetting: Setting;
+
+  /** 群列表 */
+  public group: {
+    [group_id: number]: {
+      /** 群名称 */
+      name: string;
+      /** 插件 */
+      setting: Setting;
+    };
+  };
+  /** 禁用列表 */
+  public disable: Set<string>;
+
+  constructor(
+    private bot: Bot,
+  ) {
+    const file = join(this.bot.dir, `profile-${this.bot.uin}.json`);
+
+    this.file = file;
+    this.defaultSetting = {};
+
+    try {
+      const profile = this.getLocalData();
+
+      this.group = profile.group;
+      this.disable = new Set(profile.disable);
+    } catch (error) {
+      this.group = {};
+      this.disable = new Set();
+
+      const defaultFile = {
+        group: {},
+        disable: [],
+      };
+
+      writeFileSync(file, JSON.stringify(defaultFile, null, 2));
+      this.bot.logger.info(`创建了新的配置文件：${file}`);
+    }
+
+    this.initEvents();
+  }
+
+  /**
+   * 初始化事件
+   */
+  private initEvents() {
+    this.bot.on('notice.group.increase', (event) => this.onGroupIncrease(event));
+    this.bot.on('notice.group.decrease', (event) => this.onGroupDecrease(event));
+  }
+
+  /**
+   * 定义插件默认 Option
+   * @param name - 插件名
+   * @param option - 配置项
+   */
+  public defineOption(name: string, option: Option) {
+    this.defaultSetting[name] = option;
+  }
+
+  public async refreshData(): Promise<void> {
+    const options = Object.keys(this.defaultSetting);
+    const options_length = options.length;
+
+    for (let i = 0; i < options_length; i++) {
+      const name = options[i];
+      const option = this.defaultSetting[name];
+
+      for (const [, info] of this.bot.gl) {
+        const { group_id, group_name } = info;
+
+        this.group[group_id] ??= {
+          name: group_name, setting: {},
+        };
+        this.group[group_id].name = group_name;
+        this.group[group_id].setting[name] = deepAssign(
+          deepClone(option),
+          this.group[group_id].setting[name],
+        ) as Option;
+      }
+    }
+
+    try {
+      const is_write = await this.write();
+      is_write && this.bot.logger.info(`更新了群配置`);
+    } catch (error) {
+      this.bot.logger.error(`更新群配置失败，${(<Error>error).message}`);
+    }
+  }
+
+  private async onGroupIncrease(event: MemberIncreaseEvent) {
+    if (event.user_id !== this.bot.uin) {
+      return;
+    }
+    const group_id = event.group_id;
+    const group_name = event.group.info!.group_name;
+
+    this.group[group_id] ??= {
+      name: group_name, setting: {},
+    };
+    this.group[group_id].name = group_name;
+
+    const settingNames = Object.keys(this.defaultSetting);
+    const settings_length = settingNames.length;
+
+    for (let i = 0; i < settings_length; i++) {
+      const name = settingNames[i];
+      const option = this.defaultSetting[name];
+
+      this.group[group_id].setting[name] = deepAssign(
+        option,
+        this.group[group_id].setting[name]
+      ) as Option;
+    }
+
+    try {
+      const is_write = await this.write();
+      is_write && this.bot.logger.info(`更新了群配置，新增了群：${group_id}`);
+    } catch (error) {
+      this.bot.logger.error(`更新群配置失败，${(<Error>error).message}`);
+    }
+  }
+
+  private async onGroupDecrease(event: MemberDecreaseEvent) {
+    if (event.user_id !== this.bot.uin) {
+      return;
+    }
+    const group_id = event.group_id;
+
+    delete this.group[group_id];
+
+    try {
+      const is_write = await this.write();
+      is_write && this.bot.logger.info(`更新了群配置，删除了群：${group_id}`);
+    } catch (error) {
+      this.bot.logger.error(`更新群配置失败，${(<Error>error).message}`);
+    }
+  }
+
+  // TODO ／人◕ ‿‿ ◕人＼ 使用 proxy 重构
+  private async write(): Promise<boolean> {
+    const data = {
+      group: this.group,
+      disable: [...this.disable],
+    };
+    // 数据校验，避免重复调用 writeFile
+    const localData = this.getLocalData();
+
+    if (JSON.stringify(localData) === JSON.stringify(data)) {
+      return false;
+    }
+    try {
+      await writeFile(this.file, JSON.stringify(data, null, 2));
+      return true;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private getLocalData(): Profile {
+    decache(this.file);
+    return deepClone(require(this.file));
+  }
+
+  /**
+   * 获取群插件设置。
+   * 
+   * @param group_id - 群号
+   * @returns 群插件设置
+   */
+  public getSetting(group_id: number): Setting {
+    return deepClone(this.group[group_id].setting);
+  }
+
+  /**
+   * 获取群插件配置项。
+   * 
+   * @param group_id - 群号
+   * @param name - 插件名
+   * @returns 群插件配置项
+   */
+  public getOption(group_id: number, name: string): Option {
+    return deepClone(this.group[group_id].setting[name]);
+  }
+
+  /**
+   * 获取 bot 禁用的插件。
+   * 
+   * @returns 插件禁用列表
+   */
+  public getDisablePlugins(): string[] {
+    return [...this.disable];
+  }
+
+  /**
+   * 修改插件配置项
+   * 
+   * @param group_id - 群号
+   * @param plugin - 插件名
+   * @param key - 配置项 key
+   * @param value - 配置项 value
+   * @returns 是否修改成功
+   */
+  public updateOption(group_id: number, plugin: string, key: string, value: any): Promise<boolean> {
+    const option = this.group[group_id].setting[plugin];
+    const old_value = option[key];
+    // 数据类型转换
+    const new_value = JSON.parse(value);
+
+    let message: string = '';
+
+    // 配置项类型校验
+    switch (true) {
+      case old_value === new_value:
+        message = `Error: "${key}" 当前值相等`;
+        break;
+      case !Array.isArray(old_value) && typeof old_value !== typeof new_value:
+        message = old_value
+          ? `Error: ${plugin}[${key}] 应为 ${typeof option[key]} 类型`
+          : `Error: ${key} is not defined`;
+        break;
+    }
+
+    if (message) {
+      throw new Error(message);
+    }
+    option[key] = new_value;
+
+    try {
+      return this.write();
+    } catch (error) {
+      option[key] = old_value;
+      throw error;
+    }
+  }
+}
--- src/events.ts
+++ src/events.ts
@@ -1,4 +1,7 @@
 import { DiscussMessageEvent, EventMap, GroupMessageEvent, PrivateMessageEvent } from 'oicq';
+import { Bot, PermissionLevel } from '@/core';
+import { Option } from '@/plugin';
+import { Setting } from '@/config';
 
 export type AllMessageEvent = PrivateMessageEvent | GroupMessageEvent | DiscussMessageEvent;
 
@@ -15,7 +18,7 @@ export type OmitType<T, P> = Omit<
 
 /** 获取 emit 事件回调 event 类型 */
 export type EventType<T extends (...args: any) => any> = T extends (event: infer E) => void
-  ? E extends undefined ? {} : E
+  ? (E extends undefined ? {} : E)
   : never;
 
 /** bot 事件名 */
@@ -24,4 +27,34 @@ export type EventName = keyof EventMap;
 /** bot 事件 */
 export type BotEvent<K extends EventName> = EventType<EventMap[K]> & {
   self_id: number;
+};
+
+/** 获取 emit 事件回调 context 类型 */
+export type ContextType<K extends EventName> = (
+  BotEvent<K> extends { group_id: number }
+  ? {
+    /** 群插件设置 */
+    setting: Setting;
+    /** 群配置项 */
+    option: Option;
+  }
+  : void
+)
+  & (BotEvent<K> extends { message_id: string }
+    ? {
+      /** 权限等级 */
+      permission_level: PermissionLevel;
+      /** 指令参数 */
+      query: Record<string, any>;
+    }
+    : void
+  )
+  & Record<string, any>;
+
+/** 插件上下文 */
+export type Context<K extends EventName> = BotEvent<K> & ContextType<K> & {
+  /** 快捷修改插件配置项 */
+  revise: (key: string, value: any, plugin?: string) => Promise<boolean>;
+  /** 获取 bot 实例 */
+  getBot: (uin: number) => Bot | undefined;
 };
