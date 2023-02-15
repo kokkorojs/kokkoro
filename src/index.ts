049d0c0e631a5ef54e55151bb83731ff9019ff3d
Yuki <admin@yuki.sh>
2023-02-15 14:21:31 +0800

    feat: update admin

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
+import { resolve } from 'path';
+
+declare global {
+  /** 当前进程目录 */
+  var __workname: string;
+  /** 资源目录 */
+  var __dataname: string;
+  /** 数据库目录 */
+  var __dbname: string;
+  /** 日志目录 */
+  var __logsname: string;
+}
+
+global.__workname = process.cwd();
+global.__dataname = resolve('data');
+global.__dbname = resolve('db');
+global.__logsname = resolve('logs');
+
 export * from 'oicq';
 
-export * from '@/setup';
 export * from '@/core';
 export * from '@/config';
 export * from '@/types';
 export * from '@/plugin';
+export * from '@/setup';
