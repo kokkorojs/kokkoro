65d0c9de5c5cd26fd317c7f5efe527e680ee4139
Yuki <admin@yuki.sh>
2023-10-31 18:09:17 +0800

    refactor: init monorepo

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,46 @@
+/**
+ * 创建只读 Map
+ *
+ * @param map - Map 对象
+ * @returns 只读对象
+ */
+export function createReadonlyMap<T extends Map<number | string, unknown>>(map: T): T {
+  const proxy = new Proxy(map, {
+    set() {
+      return false;
+    },
+    get(target, property, receiver) {
+      const value = Reflect.get(target, property);
+
+      if (typeof value !== 'function') {
+        return value;
+      }
+
+      switch (property) {
+        case 'set':
+        case 'clear':
+        case 'delete':
+          return () => {};
+        default:
+          return value.bind(target);
+      }
+    },
+  });
+
+  return proxy;
+}
+
+/**
+ * 检测对象是否是 Class
+ *
+ * @param value - 检测对象
+ * @returns 布尔值
+ */
+export function isClass(value: unknown): boolean {
+  if (typeof value !== 'function') {
+    return false;
+  }
+  return value.toString().startsWith('class');
+}
+
+export * from 'nanoid';
