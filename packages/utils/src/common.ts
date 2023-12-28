5f57ff5353e55db3becb1826f061e48d61c0c71b
Yuki <admin@yuki.sh>
2023-12-28 21:40:27 +0800

    feat(utils): update common

--- packages/utils/src/common.ts
+++ packages/utils/src/common.ts
@@ -44,3 +44,56 @@ export function isClass(value: unknown): boolean {
 }
 
 export function none(): void {}
+
+/**
+ * 对象转换为字符串
+ *
+ * @param value - 对象
+ * @returns 字符串结果
+ */
+export function objectToString(value: unknown): string {
+  if (typeof value === 'string') {
+    return value;
+  }
+  return JSON.stringify(value, null, 2);
+}
+
+/**
+ * 字符串转换为数字，可携带 `k` `w` `e` 单位
+ *
+ * @param value - 字符串
+ * @returns 数字结果
+ */
+export function stringToNumber(value: unknown): number {
+  if (typeof value !== 'string') {
+    return NaN;
+  }
+  const number = Number(value);
+
+  if (isNaN(number)) {
+    let float = parseFloat(value);
+    const units = value.replace(/$\d+/g, '');
+
+    for (let i = 0; i < units.length; i++) {
+      const unit = units[i];
+
+      switch (unit) {
+        case 'k':
+          float *= 1000;
+          break;
+        case 'w':
+          float *= 10000;
+          break;
+        case 'e':
+          float *= 100000000;
+          break;
+      }
+    }
+    return float;
+  }
+  return number;
+}
+
+export async function wait(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
