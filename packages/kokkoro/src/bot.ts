e1eae15d596fcb8505fc3232c5a7cb30caf48216
Yuki <admin@yuki.sh>
2024-01-26 20:27:08 +0800

    feat(kokkoro): support sandbox

--- packages/kokkoro/src/bot.ts
+++ packages/kokkoro/src/bot.ts
@@ -5,12 +5,13 @@ import { getConfig } from '@/config.js';
  * 创建机器人
  */
 export async function createBots(): Promise<void> {
-  const { log_level, bots, events } = await getConfig();
+  const { log_level, bots, events, sandbox } = await getConfig();
 
   for (let i = 0; i < bots.length; i++) {
     const config = bots[i];
 
     config.events ??= events;
+    config.sandbox ??= sandbox;
     config.log_level ??= log_level;
 
     new Bot(config).online();
