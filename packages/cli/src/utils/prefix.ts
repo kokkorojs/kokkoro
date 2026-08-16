5932e5cc270ea7ef438f1aee8fbb5b4723a92b10
Yuki <admin@yuki.sh>
2026-08-16 15:10:31 +0800

    refactor: remove legacy implementation

--- packages/cli/src/utils/prefix.ts
+++
@@ -1,6 +0,0 @@
-import { colorful } from './color.js';
-
-export const INFO = colorful(' INFO ', 'BlueBg');
-export const ERROR = colorful(' ERROR ', 'RedBg');
-export const WARN = colorful(' WARN ', 'YellowBg');
-export const SUCCESS = colorful(' SUCCESS ', 'GreenBg');
--- packages/core/src/logger.ts
+++
@@ -1,3 +0,0 @@
-import { createLogger, Logger } from 'amesu';
-
-export const logger: Logger = createLogger('kokkoro', 'INFO');
