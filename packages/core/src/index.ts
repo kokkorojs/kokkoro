6395ae8ee20d6b299d0aa7e6cc982541776efb88
Yuki <admin@yuki.sh>
2026-08-21 11:34:30 +0800

    feat(core): implement Bot runtime

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,7 @@
-Function(`"use strict";Symbol.metadata ??= Symbol('metadata');`)();
+export { Bot } from './bot';
+export { useCommand } from './command';
+export { useEvent } from './plugin';
 
-export * from '@/plugin/index.js';
-export * from '@/bot.js';
-export * from '@/logger.js';
+export type { ClientEvent, ClientEventType } from 'chobits';
+export type { Command, CommandContext, CommandHandler, CommandReply, ParseCommand } from './command';
+export type { Cleanup, Context, EventType, Plugin, PluginLoader, PluginSource } from './plugin';
